894243bb3baa44c1384821c34ca2bbbc8e926da0
Rahad Ullah <myemail.abc20@gmail.com>
2025-02-17 18:03:33 +0600

    Add logo and favicon images, update layout metadata, and enhance global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="">
-      <h1 className="text-3xl font-medium text-center">Hello</h1>
+      <h1 className="text-3xl font-medium text-center text-primary-foreground">Hello</h1>
     </div>
   );
 }
