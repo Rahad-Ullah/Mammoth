0f8405335a66981b9bf77df9addd2d3df6c191d5
Rahad Ullah <myemail.abc20@gmail.com>
2025-02-18 15:14:38 +0600

    Added auth routes to home page

--- src/app/(mainLayout)/page.tsx
+++ src/app/(mainLayout)/page.tsx
@@ -1,9 +1,37 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="">
-      <h1 className="text-3xl font-medium text-center text-primary-foreground">
-        Welcome to home page
+    <div className="py-16">
+      <div className="flex justify-center mb-6">
+        <Image src="/logo.svg" alt="Mammoth Logo" width={200} height={100} />
+      </div>
+
+      <h1 className="text-3xl font-medium text-center mb-8">
+        Welcome to <span className="text-primary-foreground">MAMMOTH</span>
       </h1>
+
+      <section className="py-16">
+        <h1 className="text-2xl font-medium text-center">
+          Authentication Pages
+        </h1>
+        <div className="flex justify-center items-center gap-6 my-6">
+          <Link href={"/login"}>
+            <Button>Login</Button>
+          </Link>
+          <Link href={"/forgot-password"}>
+            <Button>Forgot Password</Button>
+          </Link>
+          <Link href={"/reset-password"}>
+            <Button>Reset Password</Button>
+          </Link>
+          <Link href={"/otp-verify"}>
+            <Button>OTP Verify</Button>
+          </Link>
+        </div>
+      </section>
     </div>
   );
 }
