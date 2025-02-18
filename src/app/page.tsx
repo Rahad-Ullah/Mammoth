f54a755eecdcdebc684d4ca3acfcd6f1673f40df
Rahad Ullah <myemail.abc20@gmail.com>
2025-02-18 09:45:30 +0600

    Font optimized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className="">
-      <h1 className="text-3xl font-medium text-center text-primary-foreground">Hello</h1>
+      <h1 className="text-3xl font-medium text-center text-primary-foreground">
+        Test
+      </h1>
     </div>
   );
 }
