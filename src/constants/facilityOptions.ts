837cd2d0e5017716a1417ca4dbed366403f3f762
Rahad Ullah <myemail.abc20@gmail.com>
2025-02-24 12:13:37 +0600

    Added step-2 in add facility multiple steps form

---
+++ src/constants/facilityOptions.ts
@@ -0,0 +1,124 @@
+const facilityTypes = [
+  {
+    id: 1,
+    title: "To objectively diagnose small fiber neuropathy.",
+    isHidden: false,
+  },
+  {
+    id: 2,
+    title: "To diagnose inflammatory neuropathy.",
+    isHidden: false,
+  },
+  {
+    id: 3,
+    title: "To diagnose vasculitis neuropathy that also involves the skin.",
+    isHidden: false,
+  },
+  {
+    id: 4,
+    title:
+      "To provide documentation, for purposes of insurance coverage, to support recommended treatments.",
+    isHidden: false,
+  },
+  {
+    id: 5,
+    title: "To diagnose vasculitis neuropathy that also involves the skin.",
+    isHidden: false,
+  },
+  {
+    id: 6,
+    title:
+      "To provide documentation, for purposes of insurance coverage, to support recommended treatments.",
+    isHidden: false,
+  },
+  {
+    id: 7,
+    title: "To objectively diagnose small fiber neuropathy.",
+    isHidden: false,
+  },
+  {
+    id: 8,
+    title: "To diagnose inflammatory neuropathy.",
+    isHidden: false,
+  },
+  {
+    id: 9,
+    title: "To diagnose vasculitis neuropathy that also involves the skin.",
+    isHidden: true,
+  },
+  {
+    id: 10,
+    title:
+      "To provide documentation, for purposes of insurance coverage, to support recommended treatments.",
+    isHidden: true,
+  },
+];
+
+const facilityDetails = [
+  {
+    id: 1,
+    description:
+      "Pain that comes quickly can be severe but lasts a relatively short time.",
+  },
+  {
+    id: 2,
+    description: "Severe pain of limited duration.",
+  },
+  {
+    id: 3,
+    description:
+      "An unpleasant sensation induced by noxious stimuli which are detected by nerve endings of nociceptive neurons.",
+  },
+  {
+    id: 4,
+    description:
+      "Unpleasant sensory and emotional experience associated with actual or potential tissue damage.",
+  },
+  {
+    id: 5,
+    description:
+      "The sensation of discomfort, distress, or agony, resulting from the stimulation of specialized nerve endings.",
+  },
+  {
+    id: 6,
+    description:
+      "To provide documentation, for purposes of insurance coverage, to support recommended treatments.",
+  },
+  {
+    id: 7,
+    description: "Physical suffering or distress; to hurt.",
+  },
+  {
+    id: 8,
+    description: "Severe pain of limited duration.",
+  },
+  {
+    id: 9,
+    description:
+      "An unpleasant sensation induced by noxious stimuli which are detected by nerve endings of nociceptive neurons.",
+  },
+  {
+    id: 10,
+    description:
+      "Unpleasant sensory and emotional experience associated with actual or potential tissue damage.",
+  },
+  {
+    id: 11,
+    description:
+      "The sensation of discomfort, distress, or agony, resulting from the stimulation of specialized nerve endings.",
+  },
+  {
+    id: 12,
+    description:
+      "To provide documentation, for purposes of insurance coverage, to support recommended treatments.",
+  },
+  {
+    id: 13,
+    description: "Physical suffering or distress; to hurt.",
+  },
+];
+
+export const facilityOptions = {
+  facilityTypes,
+  facilityDetails,
+};
