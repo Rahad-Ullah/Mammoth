1398538270e2b7acd0fa648473cfafe2673b702a
Rahad Ullah <myemail.abc20@gmail.com>
2025-02-24 12:19:24 +0600

    Added step-3 in add facility multi step form

--- src/components/multiStepForms/addFacilityForm/Step3.tsx
+++ src/components/multiStepForms/addFacilityForm/Step3.tsx
@@ -1,12 +1,84 @@
 import { Button } from "@/components/ui/button";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { facilityOptions } from "@/constants/facilityOptions";
+import {
+  ChevronLeft,
+  ChevronRight,
+  Eye,
+  EyeOff,
+  Pencil,
+  Plus,
+  Trash,
+} from "lucide-react";
 
-const Step3 = ({ prevStep, nextStep }) => {
+const Step3 = ({ nextStep, prevStep }) => {
   // const { formData, setFormData } = useFormContext();
 
   return (
-    <div>
-      <h2>Step 3</h2>
+    <div className="h-full flex flex-col justify-between">
+      {/* header options */}
+      <section>
+        <div className="col-span-2 flex justify-end gap-4">
+          <Button onClick={prevStep} variant={"secondary"}>
+            <Plus /> Add
+          </Button>
+        </div>
+      </section>
+
+      {/* body */}
+      <section className="py-6 flex-1">
+        <ul className="grid gap-2">
+          {facilityOptions.facilityDetails.map((item) => (
+            <li
+              key={item.id}
+              className="flex justify-between items-center gap-2"
+            >
+              <p
+                className={`flex items-center gap-3 text-sm ${
+                  item.isHidden ? "text-stone-400" : "text-stone-600"
+                } `}
+              >
+                <span className="size-3 bg-primary-foreground rounded-full"></span>{" "}
+                {item.description}
+              </p>
+              <div>
+                <Button
+                  variant={"ghost"}
+                  size={"icon"}
+                  className="text-primary"
+                >
+                  <Pencil />
+                </Button>
+                {!item.isHidden && (
+                  <Button
+                    variant={"ghost"}
+                    size={"icon"}
+                    className="text-zinc-500"
+                  >
+                    <Eye />
+                  </Button>
+                )}
+                {item.isHidden && (
+                  <Button
+                    variant={"ghost"}
+                    size={"icon"}
+                    className="text-zinc-500"
+                  >
+                    <EyeOff />
+                  </Button>
+                )}
+                <Button
+                  variant={"ghost"}
+                  size={"icon"}
+                  className="text-red-500"
+                >
+                  <Trash />
+                </Button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
+
       {/* submit button */}
       <div className="col-span-2 flex justify-end gap-4">
         <Button onClick={prevStep} className="md:px-6">
--- src/constants/facilityOptions.ts
+++ src/constants/facilityOptions.ts
@@ -59,64 +59,78 @@ const facilityDetails = [
     id: 1,
     description:
       "Pain that comes quickly can be severe but lasts a relatively short time.",
+    isHidden: false,
   },
   {
     id: 2,
     description: "Severe pain of limited duration.",
+    isHidden: false,
   },
   {
     id: 3,
     description:
       "An unpleasant sensation induced by noxious stimuli which are detected by nerve endings of nociceptive neurons.",
+    isHidden: false,
   },
   {
     id: 4,
     description:
       "Unpleasant sensory and emotional experience associated with actual or potential tissue damage.",
+    isHidden: false,
   },
   {
     id: 5,
     description:
       "The sensation of discomfort, distress, or agony, resulting from the stimulation of specialized nerve endings.",
+    isHidden: false,
   },
   {
     id: 6,
     description:
       "To provide documentation, for purposes of insurance coverage, to support recommended treatments.",
+    isHidden: false,
   },
   {
     id: 7,
     description: "Physical suffering or distress; to hurt.",
+    isHidden: false,
   },
   {
     id: 8,
     description: "Severe pain of limited duration.",
+    isHidden: false,
   },
   {
     id: 9,
     description:
       "An unpleasant sensation induced by noxious stimuli which are detected by nerve endings of nociceptive neurons.",
+    isHidden: false,
   },
   {
     id: 10,
     description:
       "Unpleasant sensory and emotional experience associated with actual or potential tissue damage.",
+    isHidden: false,
   },
   {
     id: 11,
     description:
       "The sensation of discomfort, distress, or agony, resulting from the stimulation of specialized nerve endings.",
+    isHidden: true,
   },
   {
     id: 12,
     description:
       "To provide documentation, for purposes of insurance coverage, to support recommended treatments.",
+    isHidden: true,
   },
   {
     id: 13,
     description: "Physical suffering or distress; to hurt.",
+    isHidden: false,
   },
 ];
+
 
 export const facilityOptions = {
   facilityTypes,
