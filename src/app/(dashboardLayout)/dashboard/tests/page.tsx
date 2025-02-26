221790d9b364145341bd39e2df845d17172902e8
Rahad Ullah <myemail.abc20@gmail.com>
2025-02-26 09:44:26 +0600

    Filtering by status for tests added

--- src/app/(dashboardLayout)/dashboard/tests/page.tsx
+++ src/app/(dashboardLayout)/dashboard/tests/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { usersData } from "@/constants/users";
-
 import * as React from "react";
 import {
   ColumnDef,
@@ -32,8 +30,8 @@ import columns from "@/components/tableColumns/testTableColumn";
 import { TTest } from "@/types/test";
 import { testsData } from "@/constants/tests";
 
-// Extract unique roles from data
-const roles = Array.from(new Set(usersData.map((user) => user.role)));
+// Extract unique statuses from data
+const statuses = Array.from(new Set(testsData.map((test) => test.status)));
 
 const TestsPage = () => {
   const [sorting, setSorting] = React.useState<SortingState>([]);
@@ -43,15 +41,13 @@ const TestsPage = () => {
   const [columnVisibility, setColumnVisibility] =
     React.useState<VisibilityState>({});
   const [rowSelection, setRowSelection] = React.useState({});
-  const [selectedRole, setSelectedRole] = React.useState<string | null>(null);
+  const [status, setStatus] = React.useState<string | null>(null);
 
-  //   const data = React.useMemo(() => {
-  //     return selectedRole
-  //       ? usersData.filter((user) => user.role === selectedRole)
-  //       : usersData;
-  //   }, [selectedRole]);
-
-  const data = testsData;
+  const data = React.useMemo(() => {
+    return status
+      ? testsData.filter((item) => item.status === status)
+      : testsData;
+  }, [status]);
 
   const table = useReactTable<TTest>({
     data,
@@ -77,24 +73,20 @@ const TestsPage = () => {
     <div className="w-full">
       {/* table top option bar */}
       <section className="flex justify-end gap-4 items-center pb-4">
-        {/* Role Filter Dropdown */}
+        {/* Status Filter Dropdown */}
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="outline" className="capitalize">
-              {selectedRole ? `Role: ${selectedRole}` : "Filter by Role"}{" "}
-              <ChevronDown />
+              {status ? `Status: ${status}` : "Filter by Status"} <ChevronDown />
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="start">
-            <DropdownMenuItem onClick={() => setSelectedRole(null)}>
-              All Roles
+            <DropdownMenuItem onClick={() => setStatus(null)}>
+              All Status
             </DropdownMenuItem>
-            {roles.map((role) => (
-              <DropdownMenuItem
-                key={role}
-                onClick={() => setSelectedRole(role)}
-              >
-                {capitalizeSentence(role)}
+            {statuses.map((item) => (
+              <DropdownMenuItem key={item} onClick={() => setStatus(item)}>
+                {capitalizeSentence(item)}
               </DropdownMenuItem>
             ))}
           </DropdownMenuContent>
