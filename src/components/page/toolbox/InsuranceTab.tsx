d817016021d72b95f00be56165a2495b1d8db9ac
Rahad Ullah <myemail.abc20@gmail.com>
2025-04-12 16:35:21 +0600

    Implemented adding insurance

--- src/components/page/toolbox/InsuranceTab.tsx
+++ src/components/page/toolbox/InsuranceTab.tsx
@@ -1,11 +1,38 @@
+"use client";
+
 import AddModal from "@/components/add-modal";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { TabsContent } from "@/components/ui/tabs";
+import { revalidate } from "@/helpers/revalidateHelper";
 import { capitalizeSentence } from "@/utils/capitalizeSentence";
-import { Pencil, Trash } from "lucide-react";
+import { myFetch } from "@/utils/myFetch";
+import { Pencil, Plus, Trash } from "lucide-react";
+import toast from "react-hot-toast";
 
 const InsuranceTab = ({ data = [] }) => {
+  // handle add new option
+  const handleAdd = async (values: { option: string }) => {
+    toast.loading("Adding...", { id: "add-insurance" });
+    try {
+      const res = await myFetch("/insurance", {
+        method: "POST",
+        body: { name: values?.option },
+      });
+
+      if (res?.success) {
+        toast.success("Added successfully", { id: "add-insurance" });
+        await revalidate("insurance");
+      } else {
+        toast.error(res?.message || "Failed to add", { id: "add-insurance" });
+      }
+      return res || { success: false, message: "Unknown error" };
+    } catch (error) {
+      console.error(error);
+    }
+    return { success: false, message: "Unknown error" };
+  };
+
   return (
     <TabsContent
       value={"Insurance"}
@@ -16,7 +43,16 @@ const InsuranceTab = ({ data = [] }) => {
         <h1 className="text-xl lg:text-2xl font-medium text-primary">
           {capitalizeSentence("Insurance")}
         </h1>
-        <AddModal />
+        <AddModal
+          triggerBtn={
+            <Button>
+              <Plus /> Add
+            </Button>
+          }
+          title="Add New Insurance"
+          placeholderText="Enter insurance name"
+          action={handleAdd}
+        />
       </section>
       <Separator className="my-4" />
       {/* body */}
