fc1891e878cbb8fc33ff203f87c595aa7b240c9f
Rahad Ullah <myemail.abc20@gmail.com>
2025-04-21 11:38:55 +0600

    Implemented PDF and Excel button in users table

--- src/components/page/users/UsersTable.tsx
+++ src/components/page/users/UsersTable.tsx
@@ -33,6 +33,8 @@ import Image from "next/image";
 import { userRoles } from "@/constants/user-roles";
 import { IUser } from "@/types/user";
 import { useUpdateMultiSearchParams } from "@/hooks/useUpdateMultiSearchParams";
+import exportToPDF from "@/utils/exportToPdf";
+import { exportToExcel } from "@/utils/exportToExcel";
 
 // Extract unique roles from data
 const roles = Array.from(new Set(userRoles.map((item) => item.title)));
@@ -67,15 +69,52 @@ const UsersTable = ({ users = [], filters, meta }) => {
     },
   });
 
+  // handle excel button
+  const exportData = users?.map((item: IUser) => ({
+    Sl_No: item?.id,
+    Name: item?.name,
+    Phone: item?.phone,
+    Email: item?.email,
+    Address: item?.address,
+    Role: item?.role,
+  }));
+
+  // handle pdf button
+  const handleExportToPDF = () => {
+    const headers = ["Sl No", "Name", "Phone", "Email", "Address", "Role"];
+
+    const data = exportData.map((item) => [
+      item.Sl_No,
+      item.Name,
+      item.Phone,
+      item.Email,
+      item.Address,
+      item.Role,
+    ]);
+
+    exportToPDF({
+      title: "Users Table",
+      headers,
+      data,
+      fileName: "UsersTable.pdf",
+    });
+  };
+
   return (
     <div className="w-full bg-white p-4 rounded-xl h-full">
       {/* table top option bar */}
       <section className="flex flex-wrap justify-center md:justify-end gap-4 items-center pb-4">
         {/* PDF button */}
-        <Button className="bg-gradient-to-tl from-[#CEE9FF] to-[#E1E3EB] text-primary">
+        <Button
+          onClick={handleExportToPDF}
+          className="bg-gradient-to-tl from-[#CEE9FF] to-[#E1E3EB] text-primary"
+        >
           <Image src={pdfIcon} alt="pdf" width={24} height={24} />
         </Button>
-        <Button className="bg-gradient-to-tl from-[#CEE9FF] to-[#E1E3EB] text-primary">
+        <Button
+          onClick={() => exportToExcel("Users Data", exportData)}
+          className="bg-gradient-to-tl from-[#CEE9FF] to-[#E1E3EB] text-primary"
+        >
           <Image src={excelIcon} alt="pdf" width={24} height={24} />
         </Button>
 
